b73552b2e876c9f275cd290706742a46d36300e4
Akash Veeragandham <akashv@ad.unc.edu>
2022-01-23 22:54:41 -0500

    Progress on Exercise 1

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,3 +1,5 @@
-print("Hello, world.")
+"""My first program for COMP110."""
 
 __author__ = "730425241"
+
+print("Hello, world.")
---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,30 @@
+"""EX01 - Chardle - A cute step toward Wordle"""
+
+__author__ = "730425241"
+
+word: str = input("Enter a 5-character word: ")
+if len(word) != 5:
+    exit("Error: Word must contain 5 characters")
+else:
+    character: str = input("Enter a single character: ")
+    if len(character) != 1:
+        exit("Error: Character must be a single character.")
+    else:
+        print("Searching for " + character + " in " + word)
+
+if character == word[0]:
+    print(character + " found at index 0")
+else: 
+    if character == word[1]:
+        print(character + " found at index 1")
+    else:
+        if character == word[2]: 
+            print(character + " found at index 2")
+        else:
+            if character == word[3]: 
+                print(character + " found at index 3")
+            else:
+                if character == word[4]:
+                    print(character + " found at index 4")
+                else:
+                    print("No instances of " + character + " found in " + word)
