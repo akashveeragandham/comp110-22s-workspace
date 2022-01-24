3827faad790c7794bb1fbccee7c0dbd1bd2e0a0d
Akash Veeragandham <akashv@ad.unc.edu>
2022-01-24 17:59:55 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -12,13 +12,28 @@ else:
     else:
         print("Searching for " + character + " in " + word)
 
+matches: int = 0
+
 if character == word[0]:
     print(character + " found at index 0")
+    matches = matches + 1
 if character == word[1]:
     print(character + " found at index 1")
+    matches = matches + 1
 if character == word[2]: 
     print(character + " found at index 2")
+    matches = matches + 1
 if character == word[3]: 
     print(character + " found at index 3")
+    matches = matches + 1
 if character == word[4]:
     print(character + " found at index 4")
+    matches = matches + 1
+
+if matches == 1:
+    print(str(matches) + " instance of " + character + " found in " + word)
+else:
+    if matches > 1:
+        print(str(matches) + " instances of " + character + " found in " + word)
+    else: 
+        print("No instances of " + character + " found in " + word)
