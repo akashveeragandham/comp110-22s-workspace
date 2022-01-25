3d7cb001aeeb168751feed140e5d0e2a1404a2a9
Akash Veeragandham <akashv@ad.unc.edu>
2022-01-24 19:05:03 -0500

    Exercise 1 Complete

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,16 +1,18 @@
-"""EX01 - Chardle - A cute step toward Wordle"""
+"""EX01 - Chardle - A cute step toward Wordle."""
 
 __author__ = "730425241"
 
 word: str = input("Enter a 5-character word: ")
 if len(word) != 5:
-    exit("Error: Word must contain 5 characters")
-else:
-    character: str = input("Enter a single character: ")
-    if len(character) != 1:
-        exit("Error: Character must be a single character.")
-    else:
-        print("Searching for " + character + " in " + word)
+    print("Error: Word must contain 5 characters")
+    exit()
+
+character: str = input("Enter a single character: ")
+if len(character) != 1:
+    print("Error: Character must be a single character.")
+    exit()
+
+print("Searching for " + character + " in " + word)
 
 matches: int = 0
 
